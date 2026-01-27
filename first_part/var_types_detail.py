fbeb2c67cb34731cd70f735a9a3e341ac4ed1220
DumboJ <dumbojxx@gmail.com>
2026-01-28 01:37:14 +0800

    first part

---
+++ first_part/var_types_detail.py
@@ -0,0 +1,12 @@
+# python 的 + - * / // ** % 运算符
+c1 = 12
+c2 = 13
+print(c1, c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)
+print(c1 // c2)  # 向下取整
+
+print(2 ** 10)  # 1024 指数运算
+print(10 % 7)  # 3 指数运算
+
